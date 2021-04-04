3375277c02c3bbc582746bce41e44f3ab26d4247
morioprog <morio.prog@gmail.com>
2021-04-04 22:53:49 +0900

    add photos

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,106 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="(photo, idx) in photos"
+          :key="idx"
+          cols="12"
+          sm="12"
+          md="12"
+          lg="6"
+          xl="6"
+        >
+          <v-card
+            class="instaxPhotoContainer"
+            v-bind:style="{ transform: `rotate(${photo.rot})` }"
+          >
+            <img class="instaxPhoto" :src="photo.img" />
+            <span class="instaxPhotoTitle">{{ photo.name }}</span>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    photos: [
+      {
+        name: "Portfolio Site",
+        img: require("@/assets/imgs/portfolio.png"),
+        link: "https://portfolio.morio.dev/",
+        rot: "-3deg",
+      },
+      {
+        name: "Blog",
+        img: require("@/assets/imgs/blog.png"),
+        link: "https://blog.morio.dev/",
+        rot: "4deg",
+      },
+    ],
   }),
 };
 </script>
+
+<style lang="scss">
+html {
+  background-image: url("assets/imgs/bg.png");
+  background-size: auto 100vh;
+  background-repeat: repeat;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-left: 20vw;
+  margin-right: 15vw;
+  margin-top: 30vh;
+  margin-bottom: 10vh;
+  @media only screen and (max-width: 1264px) {
+    margin-left: 10vw;
+    margin-right: 8vw;
+    margin-top: 5vh;
+  }
+}
+
+.instaxPhotoContainer {
+  width: 90%;
+  filter: grayscale(100%);
+  transition: 0.5s;
+  background-color: #eeeeee;
+  margin: 5%;
+  padding-top: 5%;
+  padding-bottom: 3%;
+  &:hover {
+    filter: none;
+    box-shadow: 0 16px 80px -24px red !important;
+    transition: 0.5s;
+  }
+}
+
+.instaxPhoto {
+  width: 90%;
+  border: 1px solid #55555555;
+  margin-bottom: 3%;
+}
+
+.instaxPhotoTitle {
+  font-family: "Courier New", Courier, "American Typewriter", monospace;
+  font-weight: 600;
+  color: #dd1111;
+  font-size: 36px;
+  @media only screen and (max-width: 960px) {
+    font-size: 30px;
+  }
+  @media only screen and (max-width: 600px) {
+    font-size: 24px;
+  }
+}
+</style>
